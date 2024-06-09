7d2b28208cb91d61433e6a5611a244c8ecab8ee9
Visualnl <r.m.wagt@gmail.com>
2024-06-10 01:12:19 +0200

    changes values enemies

--- enemieslist.js
+++ enemieslist.js
@@ -1,4 +1,4 @@
 export const enemiesList = [
   { name: `Sabretooth`, health: 100, attackDmg: 3, zeni: 50, exp: 100 },
-  { name: `Dino`, health: 100, attackDmg: 3, zeni: 50, exp: 100 },
+  { name: `Dino`, health: 100, attackDmg: 10, zeni: 50, exp: 100 },
 ];
